e4528475df7fa553c8ae8815cfe3222d905bdabe
Toni Helminen <kalleankka1@gmail.com>
2020-10-12 21:02:26 +0300

    Add files via upload

--- bignum.cpp
+++ bignum.cpp
@@ -2,8 +2,9 @@
 
 #include "bignum.hpp"
 
-// "Supreme excellence consists of breaking the enemy's resistance without fighting."" -- Sun Tzu, The Art of War
+// Go
 
+// "Supreme excellence consists of breaking the enemy's resistance without fighting."" -- Sun Tzu, The Art of War
 int main(int argc, char **argv) {
   if (argc > 1) bignum::Convert(std::string(argv[1])); else bignum::PrintHelp();
   return EXIT_SUCCESS;
